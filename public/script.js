fd6a7a21281860c38c86c0fd3160522a42df8330
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-28 10:38:19 -0300

    build: catch form input value

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const inputValue = search.value;
+
+  
+
+
+
+})
